30db73f452735a5dfc554dbf8946136e9c604d08
uesleijf <uesleijf@gmail.com>
2018-06-28 20:16:50 -0300

    Django Admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from .models import Product, Category
+
+admin.site.register(Category)
+admin.site.register(Product)
--- catalog/models.py
+++ catalog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
         ordering = ['name']
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
 
