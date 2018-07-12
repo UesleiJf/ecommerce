db7c0920713ebc6a97174bcc5763ddffba590dc2
uesleijf <uesleijf@gmail.com>
2018-07-11 21:09:46 -0300

    config app

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from catalog.models import Category
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product(request):
+    return render(request, 'product.html')
