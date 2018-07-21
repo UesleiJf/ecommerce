a64aa01b10d7ca7eb63933cc0c49619d0de17231
uesleijf <uesleijf@gmail.com>
2018-07-21 15:48:13 -0300

    Melhorando a listagem de Produtos com listView

--- catalog/views.py
+++ catalog/views.py
@@ -1,25 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
 
 # Create your views here.
 
 from .models import Product, Category
 
 
-def product_list(request):
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
+
+    queryset = Product.objects.all()
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
 
 
-def category (request, slug):
-    category = Category.objects.get(slug=slug)
+product_list = ProductListView.as_view()
 
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category),
-    }
-    return render(request, 'catalog/category.html', context)
+
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+
+category = CategoryListView.as_view()
 
 
 def product(request, slug):
