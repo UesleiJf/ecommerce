f564c07d8e1f8a2dd90b05336cdc787a688d12b5
uesleijf <uesleijf@gmail.com>
2018-08-08 22:55:49 -0300

    Debug Producao

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'of0@h0##8jip41v_r*#l@u8!9mivdvq_$!u649o4m9dxv8zy*l'
 
 # Para produção Debug = True, para localhost Dbug = false
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
