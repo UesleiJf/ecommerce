6d584c1b9740d3bb2ae3974d130f892d1a840e38
uesleijf <uesleijf@gmail.com>
2018-08-10 23:05:02 -0300

    Ajstando Admin

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+
+
+class UserAdminCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UserAdminForm(forms.ModelForm):
+
+    class Meta:
+        model= User
+        fields = ['username', 'email', 'name', 'is_active', 'is_staff']
