77a6ecbe8dc613c55c3f06e02ea306048740c01b
uesleijf <uesleijf@gmail.com>
2018-09-08 18:31:29 -0300

    Realizando o Checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^carrinho/adicionar(?P<slug>[\w_-]+)/$', views.create_cartitem, name='create_cartitem'),
-    url(r'^carrinho/$', views.cart_item, name='cart_item')
+    url(
+        r'^carrinho/adicionar/(?P<slug>[\w_-]+)/$', views.create_cartitem,
+        name='create_cartitem'
+    ),
+    url(r'^carrinho/$', views.cart_item, name='cart_item'),
+    url(r'^finalizando/$', views.checkout, name='checkout')
 ]
