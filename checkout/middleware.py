335b88465ef3cd43e922547f14beb731505ccad5
uesleijf <uesleijf@gmail.com>
2018-09-18 10:57:08 -0300

    Corrigindo Middleware

--- checkout/middleware.py
+++ checkout/middleware.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 
 from .models import CartItem
 
@@ -6,9 +7,9 @@ def cart_item_middleware(get_response):
     def middleware(request):
         session_key = request.session.session_key
         response = get_response(request)
-        if session_key != request.session.session_key:
-            CartItem.objects.filter(cart_key=session_key)(
-                cart_key = request.session.session_key
+        if session_key != request.session.session_key and request.session.session_key:
+            CartItem.objects.filter(cart_key=session_key).update(
+                cart_key=request.session.session_key
             )
         return response
     return middleware
